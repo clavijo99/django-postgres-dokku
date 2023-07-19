f77890ab2b603628f6659dd07242b9f9e4963490
clavijo99 <bclavijogomez@gmail.com>
2023-07-18 21:40:57 -0500

    add jazzmin

--- project_name/urls.py
+++ project_name/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,7 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
+
 ]
+urlpatterns += i18n_patterns(path("admin/", admin.site.urls))
 
 if not settings.IS_PRODUCTION:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
